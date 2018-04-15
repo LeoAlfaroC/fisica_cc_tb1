107ef5a90a27877867fc45a13cfeefcdde4a26d3
LeoAlfaroC <leonardo14512+upc@gmail.com>
2018-04-15 18:26:10 -0500

    Organizacion de codigo :v

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 // Constante
 var g = 9.81; // Gravedad obvis
 
+var pvt = $('#pvt');
+var vvt = $('#vvt');
+var avt = $('#avt');
+var fvt = $('#fvt');
+
+var xvy_sin = $('#xvy_sin');
+var xvy_con = $('#xvy_con');
+var fvt_proy = $('#fvt_proy');
+
 
 $('#calcular_caida').click(function (e) {
     e.preventDefault();
 
-    var resultados = $('#resultados');
-    resultados.html('');
+    pvt.html('');
+    vvt.html('');
+    avt.html('');
+    fvt.html('');
 
-    var coeficiente = parseInt($('#coeficiente').val());
-    var densidad_aire = parseInt($('#densidad_aire').val());
-    var masa = parseInt($('#masa').val());
-    var area = parseInt($('#area').val());
-    var rapidez_inicial = parseInt($('#rapidez_inicial').val());
-    var tiempo_apertura_paracaidas = parseInt($('#tiempo_apertura_paracaidas').val());
-    var altura_inicial = parseInt($('#altura_inicial').val());
+    var coeficiente = parseFloat($('#coeficiente').val());
+    var densidad_aire = parseFloat($('#densidad_aire').val());
+    var masa = parseFloat($('#masa').val());
+    var area = parseFloat($('#area').val());
+    var rapidez_inicial = parseFloat($('#rapidez_inicial').val());
+    var tiempo_apertura_paracaidas = parseFloat($('#tiempo_apertura_paracaidas').val());
+    var altura_inicial = parseFloat($('#altura_inicial').val());
 
     var t = 0.0;
+    var intervalo_t = 0.01;
     var v = rapidez_inicial; // Velocidad en cada instante t
     var p = altura_inicial; // Posicion en cada instante t
     var a = g; // Aceleración en cada instante t
@@ -25,34 +37,33 @@ $('#calcular_caida').click(function (e) {
     var k = (coeficiente * densidad_aire * area) / 2;
     var v_terminal = Math.sqrt((masa * g) / k) * -1;
 
-    console.log('Vterminal: ', v_terminal.toFixed(2));
-
     // Calculamos!
-
     while (t < tiempo_apertura_paracaidas) {
         v = rapidez_inicial - (g * t);
-        //a = (v - rapidez_inicial) / 0.01;
-        //console.log(t.toFixed(2), 'velocidad', v.toFixed(4))
 
         p = altura_inicial - (g * Math.pow(t, 2) / 2);
 
-        resultados.append('<tr><td>' + t.toFixed(2) + '</td><td>' + v.toFixed(2) + '</td><td>' + a.toFixed(2) + '</td><td>' + p.toFixed(2) + '</td><td>' + f.toFixed(2) + '</td></tr>');
+        pvt.append('<tr><td>' + t.toFixed(2) + '</td><td>' + p.toFixed(2));
+        vvt.append('<tr><td>' + t.toFixed(2) + '</td><td>' + v.toFixed(2));
+        avt.append('<tr><td>' + t.toFixed(2) + '</td><td>' + a.toFixed(2));
+        fvt.append('<tr><td>' + t.toFixed(2) + '</td><td>' + f.toFixed(2));
 
-        t += 0.01;
+        t += intervalo_t;
     }
-    //console.log(v.toFixed(2), a.toFixed(2), p.toFixed(2));
 
     // v = -, a = +, p = +
     while (v <= v_terminal && p >= 0) {
-        v = v + a * 0.01;
+        v = v + a * intervalo_t;
         f = k * Math.pow(v, 2);
         a = -1 * g + ((k * Math.pow(v, 2)) / masa);
-        p = p + v * 0.01;
+        p = p + v * intervalo_t;
 
-        resultados.append('<tr><td>' + t.toFixed(2) + '</td><td>' + v.toFixed(2) + '</td><td>' + a.toFixed(2) + '</td><td>' + p.toFixed(2) + '</td><td>' + f.toFixed(2) + '</td></tr>');
-        //console.log(t.toFixed(2), v.toFixed(2), a.toFixed(2), p.toFixed(2), f.toFixed(2));
+        pvt.append('<tr><td>' + t.toFixed(2) + '</td><td>' + p.toFixed(2));
+        vvt.append('<tr><td>' + t.toFixed(2) + '</td><td>' + v.toFixed(2));
+        avt.append('<tr><td>' + t.toFixed(2) + '</td><td>' + a.toFixed(2));
+        fvt.append('<tr><td>' + t.toFixed(2) + '</td><td>' + f.toFixed(2));
 
-        t += 0.01;
+        t += intervalo_t;
     }
 });
 
@@ -72,60 +83,77 @@ $('#calcular_proyectil').click(function (e) {
     var modo = $('input[name=rozamiento]:checked').val() === 'true';
 
     var t = 0.0;
+    var intervalo_t= 0.01;
     var v = rapidez_inicial;
-    var vX_inicial = 0;
-    var vY_inicial = 0;
+
+    var vX_anterior = 0;
+    var vY_anterior = 0;
     var vX = 0;
     var vY = 0;
+    var aX = 0;
+    var aY = 0;
+    var fX = 0;
+    var fY = 0;
+    var f = 0;
 
     var posX = 0.0;
     var posY = 0.0;
 
-    vX_inicial = parseInt((Math.cos(angulo * (Math.PI / 180)) * rapidez_inicial).toFixed(2));
-    vY_inicial = parseInt((Math.sin(angulo * (Math.PI / 180)) * rapidez_inicial).toFixed(2));
-
-    vX = vX_inicial;
-    vY = vY_inicial;
-
     var radio_m = radio / 100;
     var k = (coeficiente * densidad_aire * (Math.PI * radio_m * radio_m)) / 2;
 
+    vX = parseInt((Math.cos(angulo * (Math.PI / 180)) * v).toFixed(2));
+    vY = parseInt((Math.sin(angulo * (Math.PI / 180)) * v).toFixed(2));
+
+    fX = (k * -1) * v * vX;
+    fY = masa * (g * -1) - k * v * vY;
+    f = Math.sqrt(fX * fX + fY * fY);
+
+    aX = fX / masa;
+    aY = fY / masa;
+
+
     if (!modo) {
         // Calculo sin rozamiento
+        xvy_sin.html('');
+
         do {
             // x = MRU
             posX = vX * t;
             // y = MRUV
             posY = vY * t - g * (t * t) / 2;
 
-            t += 0.01;
-
-            /*console.log('Tiempo ', t, ' Rapidez ', rapidez_inicial, 'vX ', vX, 'vY ', vY, 'V ', rapidez_inicial, 'Angulo', angulo);
-            console.log('Pos X', posX);
-            console.log('Pos Y', posY);*/
-            resultados.append('<tr><td>' + t.toFixed(2) + '</td><td>' + posX.toFixed(2) + '</td><td>' + posY.toFixed(2) + '</td></tr>');
+            t += intervalo_t;
+            xvy_sin.append('<tr><td>' + t.toFixed(2) + '</td><td>' + posX.toFixed(2) + '</td><td>' + posY.toFixed(2) + '</td></tr>');
         } while (posY >= 0);
     } else {
         // Calculo con rozamiento
+        xvy_con.html('');
+        fvt_proy.html('');
+
         do {
-            var fuerza = (k * -1) * v;
-            var fuerza_x = fuerza * vX;
-            var ax = fuerza_x / masa;
+            t += intervalo_t;
 
-            var fuerza_y =  fuerza * vY - masa * g;
-            var ay = fuerza_y / masa;
+            vX = vX + aX * intervalo_t;
+            vY = vY + aY * intervalo_t;
 
-            v = Math.sqrt((vX * vX) + (vY * vY));
+            v = Math.sqrt(vX * vX + vY * vY);
 
-            vX = vX_inicial + ax * t;
-            vY = vY_inicial + ay * t;
+            posX += (vX_anterior + vX) * intervalo_t / 2;
+            posY += (vY_anterior + vY) * intervalo_t / 2;
 
-            posX = rapidez_inicial - (ax / 2) * (t * 2 - 0.1);
-            posY = rapidez_inicial - (ay / 2) * (t * 2 - 0.1);
+            fX = (k * -1) * v * vX;
+            fY = masa * (g * -1) - k * v * vY;
+            f = Math.sqrt(fX * fX + fY * fY);
 
-            t += 0.1;
+            aX = fX / masa;
+            aY = fY / masa;
 
-            console.log(ax, ay, posX, posY);
+            vX_anterior = vX;
+            vY_anterior = vY;
+
+            xvy_con.append('<tr><td>' + t.toFixed(2) + '</td><td>' + posX.toFixed(2) + '</td><td>' + posY.toFixed(2) + '</td></tr>');
+            fvt_proy.append('<tr><td>' + t.toFixed(2) + '</td><td>' + f.toFixed(2) + '</td><td>' + fX.toFixed(2) + '</td><td>' + fY.toFixed(2) + '</td></tr>');
         } while (posY >= 0);
     }
 });
